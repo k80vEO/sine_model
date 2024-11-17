df1e00830080e7a779b2c2f4e2320ccc46309291
Kaitlyn Holbert <kholbert.2015@gmail.com>
2024-11-17 18:54:26 -0500

    Created citation file and plotting functionality for sine for main branch

---
+++ plot_sine.py
@@ -0,0 +1,21 @@
+#matplotlib inline
+#this code uses information provided on the L26 participation code and
+#from a youtube video posted by Nelson Darwin Pak Tech
+#https://www.youtube.com/watch?v=_M27BKcoWeM
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(6,6))
+xvalues = np.arange(0, 12, 0.1)
+plt.subplot(2, 2, 1)
+yvalues = np.sin(xvalues)
+plt.plot(xvalues, yvalues, color='blue')
+plt.xlabel('$x$')
+plt.ylabel('$sin(x)$')
+
+plt.grid(True)
+plt.show()
+
+plt.title('Sine Function')
+plt.tight_layout()
+plt.subplots_adjust(top=0.90)
