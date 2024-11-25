5aae95fc251003b0d90e10fc4a1d52cc5dd92b64
Kaitlyn Holbert <kholbert.2015@gmail.com>
2024-11-24 19:23:08 -0500

    Added outline for file conversion function

--- plot_sine.py
+++ plot_sine.py
@@ -3,13 +3,35 @@
 #from a youtube video posted by Nelson Darwin Pak Tech
 #https://www.youtube.com/watch?v=_M27BKcoWeM
 import numpy as np
+from scipy.signal import welch
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
+import os
+from pydub import AudioSegment
+
+file = "sound.wav"  #placeholder file name
+#function for converting to .wav
+def convert_wav():
+    split_name = os.path.splitext(file)
+    extension = split_name[1]
+    if(extension == '.m4a'):
+        #convert to wav
+        file.export("filename.wav", format = "wav")
+
+
+
+xvalues = np.arange(0, 12, 0.1)
+yvalues = np.sin(xvalues)
+
+
+
+
+
+
 
 #plt.figure(figsize=(6,6))
-xvalues = np.arange(0, 12, 0.1)
 #plt.subplot(2, 2, 1)
-yvalues = np.sin(xvalues)
+
 #plt.plot(xvalues, yvalues, color='blue')
 #plt.xlabel('$x$')
 #plt.ylabel('$sin(x)$')
